090a5a73ba75e802bd1f4eec95ab6318399c8e0c
carlos <carlos.val@bifi.es>
2014-01-09 10:58:41 +0100

    Add results admin

--- apps/results/admin.py
+++ apps/results/admin.py
@@ -3,11 +3,7 @@ from .models import Intake
 
 admin.site.register(Intake)
 
-
-
-'''
 class IntakeAdmin(admin.ModelAdmin):
     list_display = ('id', 'global_id')
 
 admin.site.register(Intake, IntakeAdmin)
-'''
