2e9f8b807b4c21b7e34a908b3d8cb8b423b7f6a5
carlos <carlos.val@bifi.es>
2014-01-14 14:43:06 +0100

    Add new urls

--- apps/survey/urls.py
+++ apps/survey/urls.py
@@ -7,11 +7,8 @@ urlpatterns = patterns('',
     url(r'^profile/electric/$', views.profile_electric, name='survey_profile_electric'),
     url(r'^main/$', views.main_index),
     url(r'^group_management/$', views.group_management, name='group_management'),
+    url(r'^survey_management/$', views.survey_management, name='survey_management'),
+    url(r'^survey_data/$', views.survey_data, name='survey_management'),
     url(r'^thanks_profile/$', views.thanks_profile, name='profile_thanks'),
-    url(r'^people/add/$', views.people_add, name='survey_people_add'),
-    url(r'^people/edit/$', views.people_edit, name='survey_people_edit'),
-    url(r'^people/remove/$', views.people_remove, name='survey_people_remove'),
-    url(r'^select/$', views.select_user, name='survey_select_user'),
     url(r'^$', views.index, name='survey_index'),
 )
-
