0338f8c66f14d6dbf43a2583ba17a8ae7d690466
carlos <carlos.val@bifi.es>
2014-01-21 15:40:47 +0100

    Modify call to personal surveis

--- apps/survey/urls.py
+++ apps/survey/urls.py
@@ -5,15 +5,17 @@ from . import views
 urlpatterns = patterns('',
     url(r'^profile/$', views.profile_index, name='survey_profile'),
     url(r'^profile/electric/$', views.profile_electric, name='survey_profile_electric'),
-    url(r'^profile/intake/$', views.survey_intake, name='survey_profile_intake'),
+    #url(r'^profile/intake/$', views.survey_intake, name='survey_profile_intake'),
     url(r'^profile/surveys/$', views.survey_management, name='survey_management'),
     url(r'^main/$', views.main_index),
-    url(r'^group_management/$', views.group_management, name='group_management'),
-    #url(r'^survey_management/$', views.survey_management, name='survey_management'),
-    url(r'^survey_data/(?P<survey_shortname>.+)/(?P<id>\d+)/$', views.survey_data, name='survey_data'),
-    #url(r'^survey_data/(?P<survey_shortname>.+)/$', views.survey_data, name='survey_data'),
-    #url(r'intake/$', views.survey_intake, name='survey_intake'),
+    url(r'^survey_management/$', views.survey_management, name='survey_management'),
+    #url(r'^survey_data/(?P<survey_shortname>.+)/(?P<id>\d+)/$', views.survey_data, name='survey_data'),
+    
+    url(r'^intake/$',              views.survey_data, name='survey_data'),
+    url(r'^monthly/(?P<id>\d+)/$', views.survey_data_monthly ,name='survey_data_monthly'),
+   
+
     url(r'^thanks_profile/$', views.thanks_profile, name='profile_thanks'),
-    url(r'^select/$', views.select_user, name='survey_select_user'),
+    #url(r'^select/$', views.select_user, name='survey_select_user'),
     url(r'^$', views.index, name='survey_index'),
 )
