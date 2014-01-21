ba12190d937706e642bb21eee91084b0d0e0aec4
carlos <carlos.val@bifi.es>
2014-01-21 15:45:01 +0100

    Add survey_data call

--- apps/pollster/urls.py
+++ apps/pollster/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     url(r'^pollster/(?P<id>\d+)/results/$', views.survey_results_csv, name='pollster_survey_results_csv'),
 
     # View data of results intake
-    url(r'^pollster/(?P<id>\d+)/data/$', views.survey_results_intake, name='pollster_survey_results_data'),
+    url(r'^pollster/(?P<id>\d+)/data/$', views.survey_data, name='pollster_survey_results_data'),
     #url(r'^pollster/(?P<shorname>.+)/$', views.survey_run, name='pollster_survey_intake'),
     url(r'^pollster/(?P<id>\d+)/publish/$', views.survey_publish, name='pollster_survey_publish'),
     url(r'^pollster/(?P<id>\d+)/unpublish/$', views.survey_unpublish, name='pollster_survey_unpublish'),
