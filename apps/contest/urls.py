adb12bf740cbf36734bd16f0a91c51dc9eca26e5
carlos <carlos.val@bifi.es>
2014-01-21 15:43:17 +0100

    Change redirect_to

--- apps/contest/urls.py
+++ apps/contest/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from django.views.generic.simple import redirect_to
+#from django.views.generic.simple import redirect_to
 from django.conf import settings
 
 from loginurl.views import cleanup, login
