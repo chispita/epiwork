fe6e51df5dbecf87be749e12bbaeba3336b5880c
carlos <carlos.val@bifi.es>
2014-01-21 15:39:46 +0100

    Modify redirect template

--- apps/tellafriend/urls.py
+++ apps/tellafriend/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'tellafriend.views.tellafriend', name="tellafriend"),
-    url(r'^success/$', 'django.views.generic.simple.direct_to_template', { 'template': 'tellafriend/success.html'}, name="tellafriend_success"),
+    #url(r'^success/$', 'django.views.generic.simple.direct_to_template', { 'template': 'tellafriend/success.html'}, name="tellafriend_success"),
 )
