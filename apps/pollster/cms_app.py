7e8f196b995def4c30799182c7fa039fc07b0e54
carlos <carlos.val@bifi.es>
2014-01-31 12:09:37 +0100

    Add SurveyMonthlyhook

--- apps/pollster/cms_app.py
+++ apps/pollster/cms_app.py
@@ -4,7 +4,12 @@ from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
 
 class SurveyIntakehook(CMSApp):
-    name = _("Intake")
+    name = _("Survey Intake")
     urls = ["apps.pollster.urls"]
-    
+
+class SurveyMonthlyhook(CMSApp):
+    name = _("Survey  Monthly")
+    urls = ["apps.pollster.urls"]
+
 apphook_pool.register(SurveyIntakehook)
+apphook_pool.register(SurveyMonthlyhook)
