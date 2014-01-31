00a497b21b9c788cb38da6c92a985e1b5c22801a
carlos <carlos.val@bifi.es>
2014-01-31 12:11:23 +0100

    Add view and update decorators

--- apps/survey/urls.py
+++ apps/survey/urls.py
@@ -5,15 +5,15 @@ from . import views
 urlpatterns = patterns('',
     url(r'^profile/$', views.profile_index, name='survey_profile'),
     url(r'^profile/electric/$', views.profile_electric, name='survey_profile_electric'),
-    #url(r'^profile/intake/$', views.survey_intake, name='survey_profile_intake'),
     url(r'^profile/surveys/$', views.survey_management, name='survey_management'),
     url(r'^main/$', views.main_index),
     url(r'^survey_management/$', views.survey_management, name='survey_management'),
-    #url(r'^survey_data/(?P<survey_shortname>.+)/(?P<id>\d+)/$', views.survey_data, name='survey_data'),
-    
-    url(r'^intake/$',              views.survey_data, name='survey_data'),
-    url(r'^monthly/(?P<id>\d+)/$', views.survey_data_monthly ,name='survey_data_monthly'),
-   
+
+    url(r'^intake/view/$', views.survey_intake_view, name='survey_intake_view'),
+    url(r'^intake/update/$', views.survey_intake_update, name='survey_intake_update'),
+
+    url(r'^monthly/(?P<id>\d+)/$', views.survey_monthly ,name='survey_monthly'),
+    url(r'^monthly/(?P<id>\d+)/update/$', views.survey_monthly_update ,name='survey_monthly_update'),
 
     url(r'^thanks_profile/$', views.thanks_profile, name='profile_thanks'),
     #url(r'^select/$', views.select_user, name='survey_select_user'),
