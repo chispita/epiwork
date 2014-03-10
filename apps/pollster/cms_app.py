60ebc3bd7b554e702d836e63026030425365c0bb
carlos <carlos.val@bifi.es>
2014-03-10 18:16:13 +0100

    Add SurveyResultshook

--- apps/pollster/cms_app.py
+++ apps/pollster/cms_app.py
@@ -11,5 +11,12 @@ class SurveyMonthlyhook(CMSApp):
     name = _("Survey  Monthly")
     urls = ["apps.pollster.urls"]
 
+class SurveyResultshook(CMSApp):
+    name = _("Survey Results")
+    urls = ["apps.survey.urls"]
+    app_name = "sublevel"
+
 apphook_pool.register(SurveyIntakehook)
 apphook_pool.register(SurveyMonthlyhook)
+apphook_pool.register(SurveyResultshook)
+
